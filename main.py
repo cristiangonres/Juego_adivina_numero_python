6003e6e617b9741a2ddef8a06fa0b0e2977ba59c
cristian <gonzalez.resina@gmail.com>
2022-12-10 14:29:46 +0100

    corrección_rango

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ numero_input = 0
 while numero != numero_input and intentos >= 0:
     
     numero_input = int(input(f'Adivina un número entre el 1 y 100: '))
-    intentos -=1
-             
-    if numero_input > 100 or numero_input < 1:
+        
+    if numero_input not in range(1,101):
         print('El número debe estar dentro del rango.')
         continue
+    
+    intentos -=1
+    
     if numero == numero_input:
             print(f'HAS GANADO. Has acertado en {8-intentos} intentos')
             break
