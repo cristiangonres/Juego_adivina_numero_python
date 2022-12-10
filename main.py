c1f837eb53f304fd84a6e2e0469e6939b2ba39fb
cristian <gonzalez.resina@gmail.com>
2022-12-10 14:15:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from random import randint
+
+intentos = 8
+numero = randint(1, 100)
+numero_input = 0
+
+while numero != numero_input and intentos >= 0:
+    
+    numero_input = int(input(f'Adivina un número entre el 1 y 100: '))
+    
+             
+    if numero_input > 100 or numero_input < 1:
+        print('El número debe estar dentro del rango.')
+        continue
+    if numero == numero_input:
+            print('Has acertado')
+    else:
+        print(f'Has fallado')
+        intentos -=1
+        if numero_input > numero:
+            print('Te has pasado, el numero es menor, sigue probando')
+        elif numero_input < numero:
+            print('El número es mayor, sigue probando')
+        print(f'Te quedan {intentos} intentos')
+else:
+    print('Has perdido la partida, eres un fracasdo')
