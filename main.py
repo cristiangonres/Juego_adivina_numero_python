4d9eb3548cac9e6833e0d4da8abad55783efeaa2
cristian <gonzalez.resina@gmail.com>
2022-12-10 14:24:57 +0100

    corrección contador

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ numero_input = 0
 while numero != numero_input and intentos >= 0:
     
     numero_input = int(input(f'Adivina un número entre el 1 y 100: '))
-    
+    intentos -=1
              
     if numero_input > 100 or numero_input < 1:
         print('El número debe estar dentro del rango.')
         continue
     if numero == numero_input:
-            print('Has acertado')
+            print(f'HAS GANADO. Has acertado en {8-intentos} intentos')
+            break
     else:
         print(f'Has fallado')
-        intentos -=1
         if numero_input > numero:
             print('Te has pasado, el numero es menor, sigue probando')
         elif numero_input < numero:
